5636307fdc05a141df89beb168f2b58e93c78c7e
Mariana Souza <Mari_rodrigue@hotmail.com>
2023-10-09 14:49:37 -0300

    Inclui configuração de Cors

--- Ecommerce/Startup.cs
+++ Ecommerce/Startup.cs
@@ -41,6 +41,8 @@ namespace Ecommerce
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(t => t.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
             app.UseSerilogRequestLogging(options =>
             {
                 options.MessageTemplate = "Processo iniciado. {RequestMethod} {RequestPath} responded {StatusCode}";
